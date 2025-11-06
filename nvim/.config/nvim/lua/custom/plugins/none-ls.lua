3dc07c233dca63896d435df2cdf00818a50b6240
Niko Pikall <nikolas.pikall@gmail.com>
2025-11-06 13:58:59 +0100

    feat(fmt): added gopls and typstyle formatters

--- nvim/.config/nvim/lua/custom/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/custom/plugins/none-ls.lua
@@ -1,52 +1,58 @@
 return {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-        "nvimtools/none-ls-extras.nvim",
-        "jayp0521/mason-null-ls.nvim",
-    },
-    config = function()
-        -- list of formatters & linters for mason to install
-        require("mason-null-ls").setup({
-            ensure_installed = {
-                "ruff",
-                "pylsp",
-                "prettier",
-                "shfmt",
-            },
-            automatic_installation = true,
-        })
+  'nvimtools/none-ls.nvim',
+  dependencies = {
+    'nvimtools/none-ls-extras.nvim',
+    'jayp0521/mason-null-ls.nvim',
+  },
+  config = function()
+    -- list of formatters & linters for mason to install
+    require('mason-null-ls').setup {
+      ensure_installed = {
+        'ruff',
+        'pyright',
+        'pylsp',
+        'prettier',
+        'shfmt',
+        'stylua',
+        'typstyle',
+        'gopls',
+      },
+      automatic_installation = true,
+    }
 
-        local null_ls = require("null-ls")
-        local sources = {
-            require("none-ls.formatting.ruff").with({
-                extra_args = {
-                    "--extend-select",
-                    "i",
-                },
-            }),
-            require("none-ls.formatting.ruff_format"),
-            null_ls.builtins.formatting.stylua,
-            null_ls.builtins.formatting.prettier.with({ filetypes = { "json", "yaml", "markdown" } }),
-            null_ls.builtins.formatting.shfmt.with({ args = { "-i", "4" } }),
-        }
+    local null_ls = require 'null-ls'
+    local sources = {
+      require('none-ls.formatting.ruff').with {
+        extra_args = {
+          '--extend-select',
+          'i',
+        },
+      },
+      require 'none-ls.formatting.ruff_format',
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.prettier.with { filetypes = { 'json', 'yaml', 'markdown' } },
+      null_ls.builtins.formatting.shfmt.with { args = { '-i', '4' } },
+      null_ls.builtins.formatting.typstyle,
+      -- null_ls.builtins.formatting.gopls,
+    }
 
-        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-        null_ls.setup({
-            sources = sources,
-            on_attach = function(client, bufnr)
-                if client.supports_method("textDocument/formatting") then
-                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-                    vim.api.nvim_create_autocmd("BufWritePre", {
-                        group = augroup,
-                        buffer = bufnr,
-                        callback = function()
-                            vim.lsp.buf.format({ async = false })
-                        end,
-                    })
-                end
+    local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+    null_ls.setup {
+      sources = sources,
+      on_attach = function(client, bufnr)
+        if client.supports_method 'textDocument/formatting' then
+          vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+          vim.api.nvim_create_autocmd('BufWritePre', {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format { async = false }
             end,
-        })
+          })
+        end
+      end,
+    }
 
-        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
+    vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
+  end,
 }
