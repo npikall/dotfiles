d89b6465b724d627be1f7f6ec47b374363e1817c
Nikolas Pikall <nikolas.pikall@gmail.com>
2026-04-21 01:24:05 +0200

    feat(nvim): allow transparent background colors in neovim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -566,6 +566,8 @@ require('lazy').setup({
     priority = 1000,
     config = function()
       vim.cmd.colorscheme 'rose-pine-moon'
+      vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
     end,
   },
 
