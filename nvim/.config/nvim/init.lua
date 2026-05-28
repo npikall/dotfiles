301e692a72032db83ee4b14792df7cc4ce83cb80
npikall <nikolas.pikall@gmail.com>
2026-05-28 15:02:24 +0200

    feat(nvim): add typstyle formatter

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -513,6 +513,7 @@ do
       python = { 'ruff' },
       json = { 'prettier' },
       markdown = { 'markdownlint' },
+      typst = { 'typstyle' },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
