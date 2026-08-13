087bd4159909f9ae4cdfd776756368221f4abc26
npikall <nikolas.pikall@gmail.com>
2026-08-13 19:14:00 +0200

    chore(nvim): update surround config

--- nvim/.config/nvim/lua/custom/plugins/surround.lua
+++ nvim/.config/nvim/lua/custom/plugins/surround.lua
@@ -1,12 +1,7 @@
-return {
-  {
-    'kylechui/nvim-surround',
-    version = '^3.0.0', -- Use for stability; omit to use `main` branch for the latest features
-    event = 'VeryLazy',
-    config = function()
-      require('nvim-surround').setup {
-        -- Configuration here, or leave empty to use defaults
-      }
-    end,
-  },
+vim.pack.add { {
+  src = 'https://github.com/kylechui/nvim-surround',
+  version = vim.version.range '4.x',
+} }
+require('nvim-surround').setup {
+  -- Put your configuration here
 }
