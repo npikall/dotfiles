2aaaf42c81f81a03a585debb30591d528a54e86f
npikall <nikolas.pikall@gmail.com>
2026-09-12 00:16:38 +0200

    refactor(nvim): remove stale plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -649,20 +649,10 @@ do
 end
 
 -- ============================================================
--- SECTION 9: OPTIONAL EXAMPLES / NEXT STEPS
--- kickstart.plugins.* examples
+-- SECTION 9: MODULES
+-- kickstart.plugins.* and custom.plugins.*
 -- ============================================================
 do
-  -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- place them in the correct locations.
-
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
-  -- require 'kickstart.plugins.debug'
   require 'kickstart.plugins.indent_line'
   require 'kickstart.plugins.lint'
   require 'kickstart.plugins.autopairs'
